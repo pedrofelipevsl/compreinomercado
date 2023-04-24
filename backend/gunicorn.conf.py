0efdd34e8cf66205e6750a4c23a6972ea6162493
Pedro Lima <pedro.lima@external.toyota-europe.com>
2023-04-24 18:21:08 -0300

    adjusting the project

--- backend/gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-workers = 4  # Número de workers para o Gunicorn
-module_name = 'pedrofelipevsl_pythonanywhere_com_wsgi.py:application'  # Nome do arquivo do aplicativo Flask (wsgi.py) e instância do aplicativo (application)
-
-bind = '0.0.0.0:8000'  # Endereço IP e porta para a execução do Gunicorn
